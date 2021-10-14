10d252534c0227be4d7e9028eda6782a4a7b0876
Sameer Rai <sameer.rai@nectechnologies.in>
2021-10-14 12:26:52 +0530

    Adding movement to both left and right

--- alien_invasion.py
+++ alien_invasion.py
@@ -23,6 +23,7 @@ class AlienInvasion:
         """Start the main loop for the game"""
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
 
             # Make the most recently drawn screen visible
@@ -36,7 +37,15 @@ class AlienInvasion:
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
                     # Move the ship to right
-                    self.ship.rect.x += 1
+                    # self.ship.rect.x += 1
+                    self.ship.moving_right=True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left=True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right=False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left=False
 
     def _update_screen(self):
         """Update images on the sceen and flip to the new screen """
